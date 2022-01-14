dacc372c99c6462896f06b60a4b78a347cdf2a47
dreadwing5 <sachinspyk@outlook.com>
2022-01-14 22:30:00 +0530

    Added faculty routes

---
+++ controller/facultyController.js
@@ -0,0 +1,34 @@
+exports.createFaculty = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!!",
+  });
+};
+
+exports.getAllFaculties = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!!",
+  });
+};
+
+exports.getFaculty = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!!",
+  });
+};
+
+exports.updateFaculty = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!!",
+  });
+};
+
+exports.deleteFaculty = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!!",
+  });
+};
